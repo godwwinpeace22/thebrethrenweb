c168afac8a367e5036d40ed3fc1178d5bdd955a1
godwwinpeace22 <godwinpeace22@gmail.com>
2017-06-07 20:07:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+		
+	$(".three-d").hover(function(){
+		var myArray = ["mount-everest-himalayas-nuptse-lhotse-51387.jpeg",
+		"pexels-photo-128458.jpeg","pexels-photo-169738.jpeg","pexels-photo-248797.jpeg"];
+	
+		$(".slideshow").css("background-image", "url('" + myArray[Math.floor((Math.random() * 4) + 0)] + "')");
+		return false;
+	})
+	// Perform the slide button effect on mobile devices!
+	$(".three-d").on("swiperight",function(){
+  	//alert("You swiped right!");
+	});
+
+	$(window).mousemove(function(event) {
+  	$(".slideshow").animate({left : event.pageX/100, top : event.pageY/100},10);
+	});
+
+
+
+
+
+	var slideindex = 0
+	function showSlides(){
+	var slides = document.getElementsByClassName("tooltip");
+	for(i=0; i<slides.length; i++){
+	slides[i].style.display = "none";
+	
+	}
+	slideindex++;
+	if(slideindex>slides.length){slideindex = 1}
+	$(slides[slideindex -1]).fadeIn(3000).fadeOut(5000);
+	setTimeout(showSlides,8000);
+	//$(".tooltip").fadeOut(100).fadeIn("slow");
+	
+	
+	
+	}
+	showSlides();
+
+
+	$(".menu1").click(function(event){
+		$(".slidedowncont").slideDown("slow");
+		$(".menu1").hide("slow");
+		$(".menu2").fadeIn("slow");
+		//$(".slidedown").css("height","500px");
+		return false;
+	})
+
+	$(".menu2").click(function(event){
+		$(".slidedowncont").slideUp("slow");
+		$(".menu2").hide("slow");
+		$(".menu1").fadeIn("slow");
+		//$(".slidedown").css("height","500px");
+		return false;
+	})
+
+})
+
+	
