daea13f3fb48df155105d009e047004fa454eb15
godwwinpeace22 <godwinpeace22@gmail.com>
2017-06-10 16:30:29 +0100

    Create script.js

--- script.js
+++ script.js
@@ -1,42 +1,23 @@
+
+
 $(document).ready(function(){
-		
 	$(".three-d").hover(function(){
 		var myArray = ["mount-everest-himalayas-nuptse-lhotse-51387.jpeg",
-		"pexels-photo-128458.jpeg","pexels-photo-169738.jpeg","pexels-photo-248797.jpeg"];
+		"01.jpg","6.jpg","pexels-photo-248797.jpeg"];
 	
 		$(".slideshow").css("background-image", "url('" + myArray[Math.floor((Math.random() * 4) + 0)] + "')");
 		return false;
+
+
+
 	})
-	// Perform the slide button effect on mobile devices!
-	$(".three-d").on("swiperight",function(){
-  	//alert("You swiped right!");
-	});
+	// Perform the slide button effect on mobile devices
+
+	
 
 	$(window).mousemove(function(event) {
-  	$(".slideshow").animate({left : event.pageX/100, top : event.pageY/100},10);
+  		$(".slideshow").animate({left : event.pageX/100, top : event.pageY/100},10);
 	});
-
-
-
-
-
-	var slideindex = 0
-	function showSlides(){
-	var slides = document.getElementsByClassName("tooltip");
-	for(i=0; i<slides.length; i++){
-	slides[i].style.display = "none";
-	
-	}
-	slideindex++;
-	if(slideindex>slides.length){slideindex = 1}
-	$(slides[slideindex -1]).fadeIn(3000).fadeOut(5000);
-	setTimeout(showSlides,8000);
-	//$(".tooltip").fadeOut(100).fadeIn("slow");
-	
-	
-	
-	}
-	showSlides();
 
 
 	$(".menu1").click(function(event){
@@ -55,6 +36,31 @@ $(document).ready(function(){
 		return false;
 	})
 
+	var slideindex = 0
+	function showSlides(){
+	var slides = document.getElementsByClassName("tooltip");
+	for(i=0; i<slides.length; i++){
+	slides[i].style.display = "none";
+	
+	}
+	slideindex++;
+	if(slideindex>slides.length){slideindex = 1}
+	$(slides[slideindex -1]).fadeIn(3000).fadeOut(5000);
+	setTimeout(showSlides,8000);
+	}
+	showSlides();
+	
 })
+	
+window.onload = function(argument) {
+	//$("div").hide();
+	//$("body").fadeIn(2000);
+	$(".menu1").animate({"top": "10px", "opacity":"1"},2000);
+	$(".blocklist").animate({"margin-left": "0px", "opacity":"1"},2000);
+	//$(".blocklist1").animate({"margin-left": "0px", "opacity":"1"},2000);
+	$("body").animate({"opacity":"1"},2000);
+}
+
+	
 
 	
